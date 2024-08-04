ba8bf75f3782b249eed8cf9d9eae5290ba2942ba
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-08-04 19:01:35 +0100

    fix(navbar): navbar transitions

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -23,9 +23,10 @@ const Menu = ({ isVisible }: Props) => {
 
 	return (
 		<motion.div
-			className="border-b-shadow border-b-xs"
+			className="bg-ghost absolute w-full border-shadow border-b-xs lg:right-0 lg:w-1/4 lg:border-l-xs lg:rounded-bl-md"
 			initial={{ opacity: 0 }}
-			animate={{ opacity: isVisible ? 1 : 0 }}
+			animate={{ opacity: isVisible ? 1 : 0, transition: { duration: 0.5 } }}
+			exit={{ opacity: 0, transition: { duration: 0.5 } }}
 		>
 			<div className="bg-ghost w-full">
 				{links.map(({ name, href }) => (
@@ -33,7 +34,7 @@ const Menu = ({ isVisible }: Props) => {
 						<motion.div
 							className="flex border-shadow hover:border-sky border-xs items-center text-lg m-4 rounded-md h-16"
 							initial={{ scale: 0 }}
-							animate={{ scale: 1 }}
+							animate={{ scale: 1, transition: { duration: 0.5 } }}
 							transition={{
 								type: "spring",
 								stiffness: 260,
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -12,9 +12,9 @@ const NavBar = () => {
 
 	return (
 		<>
-			<nav className="flex items-center border-b-shadow border-b-xs h-16 sticky top-0 w-full">
+			<nav className="flex items-center bg-ghost border-b-shadow border-b-xs h-16 w-full">
 				<a href="/" className="ml-4 flex-grow">
-					<div className="flex border-shadow border-xs rounded-full items-center">
+					<div className="flex border-shadow border-xs rounded-full w-36 items-center">
 						<div className="w-4 h-4 bg-snow rounded-full m-3" />
 						<div className="text-lg pr-3">mzyxnuel</div>
 					</div>
