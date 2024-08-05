730f0747fac2a785ed46ea7904246c0b632eafc0
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-08-05 21:56:07 +0100

    fix(SEO): update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 export const metadata: Metadata = {
 	title: "mzyxnuel",
 	description: "manuel's personal website",
+	category: "personal website portfolio",
+	keywords: ["web developer", "software engineer", "portfolio", "personal website", "backend developer", "frontend developer", "sossai", "manuel", "sossai manuel", "mzyxnuel"],
 };
 
 const hanken = Hanken_Grotesk({
